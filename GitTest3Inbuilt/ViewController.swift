e695774ffd2ac8b0380c63976df3dc5fbc5e8834
Saurabh Chandra Bose <bose008@gmail.com>
2019-07-19 13:11:33 +0530

    Add viewDidAppear

--- GitTest3Inbuilt/ViewController.swift
+++ GitTest3Inbuilt/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 
 
 }
