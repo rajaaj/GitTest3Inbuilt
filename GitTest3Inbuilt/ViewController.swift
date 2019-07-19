d8ae0cf3292ce7a8df5c4ea02138b86ee50481c6
Saurabh Chandra Bose <bose008@gmail.com>
2019-07-19 13:14:06 +0530

    remove viewWillAppear and add viewDidAppear

--- GitTest3Inbuilt/ViewController.swift
+++ GitTest3Inbuilt/ViewController.swift
@@ -17,12 +17,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
     }
 
 
